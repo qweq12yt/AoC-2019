1f245151934a24118890978a5c3a46e321616fee
qweq12yt <45515587+qweq12yt@users.noreply.github.com>
2019-12-10 19:04:59 -0300

    tried second half of day10, cucked by floats

--- day10.py
+++ day10.py
@@ -1,9 +1,33 @@
 from aocutil import file_input
-from math import gcd
+from math import gcd, atan, degrees
 from copy import deepcopy
 
 
-def solve_part1(input_file_name):
+def get_angle(center, target):
+    target = list(target)
+    target[0] -= center[0]
+    target[1] -= center[1]
+    try:
+        angle = degrees(atan(target[0] / target[1]))
+    except ZeroDivisionError:
+        if target[0] > 0:
+            return 90
+        else:
+            return -90 + 360
+    if angle < 0:
+        return angle + 360
+    else:
+        return angle
+
+
+def to_clockwise(angle):
+    angle -= 270
+    if angle < 0:
+        return angle + 360
+    return angle
+
+
+def solve_day10(input_file_name):
     data = file_input(input_file_name)
     asteroids = []
     for i in range(len(data)):
@@ -13,7 +37,14 @@ def solve_part1(input_file_name):
     h = []
     for asteroid in asteroids:
         h.append(asteroids_in_range(asteroids, asteroid, data))
-    return max(h, key=lambda x:x[0])[0]
+    part1, station, asteroids = max(h, key=lambda x: x[0])
+    clockwise = []
+    for asteroid in asteroids:
+        clockwise.append((list(reversed(asteroid)), to_clockwise(get_angle(station, asteroid))))
+    clockwise.sort(key=lambda x: x[1])
+
+    # return part1, list(list(clockwise[10 -1][0]).__reversed__()), station
+    return part1
 
 
 def asteroids_in_range(asteroids, asteroid, data):
@@ -48,9 +79,13 @@ def asteroids_in_range(asteroids, asteroid, data):
                         break
                 # I told you :/
 
-    visible = len(set(asteroids) - set(blind_spots)) - 1
-    to_return[0] = visible
-    to_return[1] = [asteroid[1], asteroid[0]]
+    to_return[1] = [asteroid[0], asteroid[1]]   # (y, x)
     to_return[2] = set(asteroids) - set(blind_spots)
+    to_return[2].remove(tuple(to_return[1]))
+    to_return[0] = len(to_return[2])
 
     return to_return   # [0] is the quantity, [1] is what asteroid is the chosen one. [2] is a set of all VISIBLE asteroids
+
+
+print(solve_day10('day10.txt'))
+
